8562991c5c34ab0231c8780f32d2372a9cf5780e
davidspz1989 <david.spz1989@gmail.com>
2023-04-27 21:50:27 -0600

    agrega clases, Categoria, Examen, Pregunta

---
+++ src/main/java/com/sistema/examenes/entidades/Categoria.java
@@ -0,0 +1,60 @@
+package com.sistema.examenes.entidades;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoriaId;
+
+    private String titulo;
+    private String descripcion;
+
+    /*
+    LinkedHashSet
+    garantiza el orden de los elementos almacenados,
+    no permite elementos duplicados,
+    permite recorrer de forma ordenada y de forma inversa
+    */
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    private Set<Examen>examenes=new LinkedHashSet<>();
+
+    public Long getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Long categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Set<Examen> getExamenes() {
+        return examenes;
+    }
+
+    public void setExamenes(Set<Examen> examenes) {
+        this.examenes = examenes;
+    }
+
+    public Categoria(){}
+}
---
+++ src/main/java/com/sistema/examenes/entidades/Pregunta.java
@@ -0,0 +1,90 @@
+package com.sistema.examenes.entidades;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "preguntas")
+public class Pregunta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long preguntaId;
+    @Column(length = 500)
+    private String contenido;
+    private String imagen;
+
+    private String opcion1;
+    private String opcion2;
+    private String opcion3;
+    private String opcion4;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Examen examen;
+
+    public Long getPreguntaId() {
+        return preguntaId;
+    }
+
+    public void setPreguntaId(Long preguntaId) {
+        this.preguntaId = preguntaId;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getOpcion1() {
+        return opcion1;
+    }
+
+    public void setOpcion1(String opcion1) {
+        this.opcion1 = opcion1;
+    }
+
+    public String getOpcion2() {
+        return opcion2;
+    }
+
+    public void setOpcion2(String opcion2) {
+        this.opcion2 = opcion2;
+    }
+
+    public String getOpcion3() {
+        return opcion3;
+    }
+
+    public void setOpcion3(String opcion3) {
+        this.opcion3 = opcion3;
+    }
+
+    public String getOpcion4() {
+        return opcion4;
+    }
+
+    public void setOpcion4(String opcion4) {
+        this.opcion4 = opcion4;
+    }
+
+    public Examen getExamen() {
+        return examen;
+    }
+
+    public void setExamen(Examen examen) {
+        this.examen = examen;
+    }
+
+    public Pregunta(){}
+}
